9ea28006b0703c59a0055cc659e315648f568fed
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-03-16 17:35:20 +0800

    LC-minimum-time-to-repair-cars [Runtime: N/A; Memory: N/A]

--- binary-search/minimum-time-to-repair-cars.java
+++ binary-search/minimum-time-to-repair-cars.java
@@ -1,12 +1,22 @@
 class Solution {
     public long repairCars(int[] ranks, int cars) {
-        long left = 0, right = (long) Arrays.stream(ranks).min().getAsInt() * cars * cars;
+        TreeMap<Integer, Integer> freqMap = new TreeMap<>();
+        // Improve efficiency by passing a frequency array of ranks rather than ranks itself
+        for (int rank : ranks) {
+            freqMap.put(rank, freqMap.getOrDefault(rank, 0) + 1);
+        }
+        
+        long left = 0, right = (long) 1e14;
         while (left < right) {
-            int count = 0;
             long mid = (left + right) / 2;
-            for (int rank : ranks) {
-                count += Math.sqrt(mid / rank);
+            long count = 0;
+
+            for (var entry : freqMap.entrySet()) {
+                int rank = entry.getKey(), freq = entry.getValue();
+                count += freq * Math.sqrt(mid / rank);
+                if (count >= cars) break; 
             }
+
             if (count >= cars) right = mid;
             else left = mid + 1;
         }
