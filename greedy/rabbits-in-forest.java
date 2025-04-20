ce05c7674c28c624180b5bd7409a85eb2a109171
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-04-20 18:00:11 +0800

    LC-rabbits-in-forest [Runtime: N/A; Memory: N/A]

--- greedy/rabbits-in-forest.java
+++ greedy/rabbits-in-forest.java
@@ -5,10 +5,9 @@ class Solution {
         for (int i : answers) map.put(i, map.getOrDefault(i, 0) + 1);
         for (int key : map.keySet()) {
             int value = map.get(key);
-            while (value > 0) {
-                ans += key + 1;
-                value -= key + 1;
-            }
+            int groupSize = key + 1;
+            int group = Math.max(value / groupSize, 1);
+            ans += group * groupSize;
         }
         return ans;
     }
