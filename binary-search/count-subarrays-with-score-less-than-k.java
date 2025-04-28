1e994d9e75111b682985916defb2e966002f933a
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-04-28 20:03:51 +0800

    LC-count-subarrays-with-score-less-than-k [Runtime: 2 ms; Memory: 61.5 MB]

--- binary-search/count-subarrays-with-score-less-than-k.java
+++ binary-search/count-subarrays-with-score-less-than-k.java
@@ -1,25 +1,15 @@
 class Solution {
+    // The score of the subarray in the window is sum * (i - j + 1). 
+    // We move the left side of the window, decreasing sum, 
+    // if that score is equal or greater than k.
     public long countSubarrays(int[] nums, long k) {
-        long ans = 0;
-        int len = nums.length;
-        long[] prefix = new long[len+1];
-        for (int i = 0; i < len; i++) {
-            prefix[i+1] = prefix[i] + nums[i];
-        }
-        for (int i = 0; i < len; i++) {
-            int left = i, right = len;
-            while (left < right) {
-                int mid = left + (right - left) / 2;
-                long sum = prefix[mid+1] - prefix[i];
-                long score = sum * (mid - i + 1);
-                if (score < k) {
-                    left = mid + 1;
-                } else {
-                    right = mid;
-                }
-            }
-            ans += left - i;
-        }
-        return ans;
+    long sum = 0, res = 0;
+    for (int left = 0, right = 0; right < nums.length; right++) {
+        sum += nums[right];
+        while (sum * (right - left + 1) >= k)
+            sum -= nums[left++];
+        res += right - left + 1;
     }
+    return res;
+ }
 }
