18db092b0dbd55141d77ec90ef6bf8212cda7a49
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-05-04 18:58:04 +0800

    LC-bulls-and-cows [Runtime: 5 ms; Memory: 42.7 MB]

--- hash-table/bulls-and-cows.java
+++ hash-table/bulls-and-cows.java
@@ -1,20 +1,18 @@
 class Solution {
     public String getHint(String secret, String guess) {
-        int[] count1 = new int[10];
-        int[] count2 = new int[10];
+        int[] count = new int[10];
         int bull = 0, cow = 0;
         for (int i = 0; i < secret.length(); i++) {
-            char c1 = secret.charAt(i);
-            char c2 = guess.charAt(i);
-            if (c1 == c2) {
+            int s = secret.charAt(i) - '0';
+            int g = guess.charAt(i) - '0';
+            if (s == g) {
                 bull++;
-                continue;
+            } else {
+                if (count[s] < 0) cow++;
+                if (count[g] > 0) cow++;
+                count[s]++;
+                count[g]--;
             }
-            count1[c1 - '0']++;
-            count2[c2 - '0']++;
-        }
-        for (int i = 0; i < 10; i++) {
-            cow += Math.min(count1[i], count2[i]);
         }
         return bull + "A" + cow + "B";
     }
