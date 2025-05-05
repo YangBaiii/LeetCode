7ee71a7f33ea3f175392f7d64442c6116d89fe87
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-05-05 23:15:50 +0800

    LC-domino-and-tromino-tiling [Runtime: N/A; Memory: N/A]

--- dynamic-programming/domino-and-tromino-tiling.java
+++ dynamic-programming/domino-and-tromino-tiling.java
@@ -10,7 +10,7 @@ class Solution {
         int[] dp = new int[1001];
         int mod = 1000000007, ans = 0;
         dp[1] = 1;
-        dp[2] = 3;
+        dp[2] = 2;
         dp[3] = 5;
         for (int i = 4; i <= n; i++) {
             dp[i] = 2 * dp[i-1] + dp[i-3];
