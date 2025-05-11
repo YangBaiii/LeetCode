9351b3d4c868480c362a20b956ed824ed15761f4
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-05-11 19:39:47 +0800

    LC-minimum-equal-sum-of-two-arrays-after-replacing-zeros [Runtime: 3 ms; Memory: 60.9 MB]

--- array/minimum-equal-sum-of-two-arrays-after-replacing-zeros.java
+++ array/minimum-equal-sum-of-two-arrays-after-replacing-zeros.java
@@ -1,32 +1,16 @@
 class Solution {
     public long minSum(int[] nums1, int[] nums2) {
-        int len1 = nums1.length, len2 = nums2.length, min = Math.min(len1, len2);
-        long count1 = 0, count2 = 0, diff = 0, sum1 = 0, sum2 = 0;
-        for (int i = 0; i < Math.max(len1, len2); i++) {
-            if (i < min) {
-                if (nums1[i] == 0) count1++;
-                if (nums2[i] == 0) count2++;
-                sum1 += nums1[i];
-                sum2 += nums2[i];
-                diff += nums1[i] - nums2[i];
-            } else {
-                if (i < len1) {
-                    if (nums1[i] == 0) count1++;
-                    sum1 += nums1[i];
-                    diff += nums1[i];
-                } else {
-                    if (nums2[i] == 0) count2++;
-                    sum2 += nums2[i];
-                    diff -= nums2[i];
-                }
-            }
+        long sum1 = 0, sum2 = 0, count1 = 0, count2 = 0;
+        for (int i : nums1) {
+            count1 += i == 0 ? 1 : 0;
+            sum1 += Math.max(1, i);
         }
-        if (diff == 0 && count1 == 0 && count2 == 0) return sum1;
-        if (sum1 > sum2) {
-            if (count2 == 0 || (count1 == 0 && diff < count2)) return -1;
-        } else {
-            if (count1 == 0 || (count2 == 0 && Math.abs(diff) < count1)) return -1;
+        for (int i : nums2) {
+            count2 += i == 0 ? 1 : 0;
+            sum2 += Math.max(1, i);;
         }
-        return (long)(Math.max(sum1 + count1, sum2 + count2));
+        if (count1 == 0 && sum1 < sum2) return -1;
+        if (count2 == 0 && sum1 > sum2) return -1;
+        return Math.max(sum1, sum2);
     }
 }
