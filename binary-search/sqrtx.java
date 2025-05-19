9dc24ea86eb8b10e9fd7eccc9bbb3a24ce19b1e1
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-05-19 17:44:42 +0800

    LC-sqrtx [Runtime: N/A; Memory: N/A]

---
+++ binary-search/sqrtx.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x == 0 || x == 1) return x;
+        int left = 1, right = x;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (mid * mid > x) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            } 
+        }
+        if(x / right >= right) return right;
+        return left;
+    }
+}
