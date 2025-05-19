7ff8147ecdf987b934ccd2eaedcea4a619187f60
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-05-19 17:56:26 +0800

    LC-valid-perfect-square [Runtime: N/A; Memory: N/A]

---
+++ binary-search/valid-perfect-square.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if (num == 1) return true;
+        int left = 0, right = num;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (mid < num / mid) {
+                left = mid + 1;
+            } else if (mid > num / mid) {
+                right = mid - 1;
+            } else return true;
+        }
+        return false;
+    }
+}
