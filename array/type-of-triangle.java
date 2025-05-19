496f69dde02aba995e54cf34275d2a1dd951310f
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-05-19 16:09:06 +0800

    LC-type-of-triangle [Runtime: 0 ms; Memory: 41.9 MB]

--- array/type-of-triangle.java
+++ array/type-of-triangle.java
@@ -2,10 +2,10 @@ class Solution {
     public String triangleType(int[] nums) {
         if (nums[1] == nums[0]) {
             if (nums[1] == nums[2]) return "equilateral";
+            return canFormTriangle(nums[0], nums[1], nums[2]) ? "isosceles" : "none";
         } else if (nums[2] == nums[1] || nums[2] == nums[0]) {
             return canFormTriangle(nums[0], nums[1], nums[2]) ? "isosceles" : "none";
         } else return canFormTriangle(nums[0], nums[1], nums[2]) ? "scalene" : "none";
-        return null;
     }
 
     public boolean canFormTriangle(int a, int b, int c) {
