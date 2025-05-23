0a2aa343cd7900c3c5eed292edf47a6ecbd0de60
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-05-23 16:31:44 +0800

    LC-intersection-of-two-linked-lists [Runtime: N/A; Memory: N/A]

---
+++ two-pointers/intersection-of-two-linked-lists.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int len1 = 0, len2 = 0;
+        ListNode head1 = headA, head2 = headB;
+        while (headA != null) {
+            headA = headA.next;
+            len1++;
+        }
+        while (headB != null) {
+            headB = headB.next;
+            len2++;
+        }
+        if (len1 > len2) {
+            while (n < len1 - len2) {
+                head1 = head1.next;
+                n++;
+            }
+        } else {
+            while (n < len2 - len1) {
+                head2 = head2.next;
+                n++;
+            }
+        }
+        while (head1 != null) {
+            if (head1.val == head2.val && head1.next == head2.next) return head1;
+            head1 = head1.next;
+            head2 = head2.next;
+        }
+        return null;
+    }
+}
