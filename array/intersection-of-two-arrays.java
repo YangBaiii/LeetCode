77828c3f8c9a2c72b4d4c27cd8433fae3ae3397a
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-05-25 18:50:14 +0800

    LC-intersection-of-two-arrays [Runtime: N/A; Memory: N/A]

---
+++ array/intersection-of-two-arrays.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        int[] count = new int[10];
+        List<Integer> list = new ArrayList<>();
+        for (int i : nums1) count[i]++;
+        for (int i : nums2) {
+            if (count[i] != 0) {
+                list.add(i);
+                count[i] = 0;
+            }
+        }
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
