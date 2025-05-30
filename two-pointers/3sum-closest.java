1cb8ccf7757e9537fa55b5cbde157b0844bb524c
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-05-30 21:51:13 +0800

    LC-3sum-closest [Runtime: N/A; Memory: N/A]

---
+++ two-pointers/3sum-closest.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        int diff = 10001, ans = 0;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+            int left = i + 1, right = nums.length - 1;
+            while (left < right) {
+                int count = nums[i] + nums[left] + nums[right];
+                if (count == target) return count;
+                else if (count < target) left++;
+                else right--;
+                if (Math.abs(count - target) < diff) {
+                    ans = count;
+                    diff = Math.abs(count - target);
+                    left++;
+                    right--;
+                } 
+            }
+        }
+        return ans;
+    }
+}
