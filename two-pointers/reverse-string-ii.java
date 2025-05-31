dfab4cf833f7c96020430e3e9edaf130bc4646ad
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-05-31 17:37:20 +0800

    LC-reverse-string-ii [Runtime: 1 ms; Memory: 45.1 MB]

--- two-pointers/reverse-string-ii.java
+++ two-pointers/reverse-string-ii.java
@@ -1,14 +1,15 @@
 class Solution {
     public String reverseStr(String s, int k) {
-        if (s.length() <= k) {
-            StringBuilder ans = new StringBuilder(s);
-            return ans.reverse().toString();
-        }
         StringBuilder ans = new StringBuilder();
         for (int i = 0; i < s.length(); i += 2 * k) {
-            StringBuilder sb = new StringBuilder(s.substring(i,i+k));
+            int end = Math.min(i + k, s.length());
+            StringBuilder sb = new StringBuilder(s.substring(i, end));
             ans.append(sb.reverse());
-            ans.append(s.substring(i+k, Math.min(i+k+k, s.length())));
+
+            if (end < s.length()) {
+                int nextEnd = Math.min(end + k, s.length());
+                ans.append(s.substring(end, nextEnd));
+            }
         }
         return ans.toString();
     }
