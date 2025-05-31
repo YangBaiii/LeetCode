45b35b563e2c4262cd3dd38a169329862104a270
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-05-31 17:35:26 +0800

    LC-reverse-string-ii [Runtime: N/A; Memory: N/A]

--- string/reverse-string-ii.java
+++ string/reverse-string-ii.java
@@ -1,5 +1,9 @@
 class Solution {
     public String reverseStr(String s, int k) {
+        if (s.length() <= k) {
+            StringBuilder ans = new StringBuilder(s);
+            return ans.reverse().toString();
+        }
         StringBuilder ans = new StringBuilder();
         for (int i = 0; i < s.length(); i += 2 * k) {
             StringBuilder sb = new StringBuilder(s.substring(i,i+k));
