a35b9d86f5b8894d9f72a53b07a67fa296f3ee53
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-06-03 16:31:54 +0800

    LC-maximum-candies-you-can-get-from-boxes [Runtime: N/A; Memory: N/A]

---
+++ array/maximum-candies-you-can-get-from-boxes.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int maxCandies(int[] status, int[] candies, int[][] keys, int[][] containedBoxes, int[] initialBoxes) {
+        int ans = 0, n = candies.length;
+        boolean[] usedBoxes = new boolean[n]; 
+        boolean[] boxFound = new boolean[n];
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i : initialBoxes) {
+            if (status[i] == 1) queue.add(i);
+            boxFound[i] = true;
+        }
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            if (status[cur] == 1 && !usedBoxes[cur]) {
+                ans += candies[cur];
+                usedBoxes[cur] = true;
+            }
+            for (int i : keys[cur]) {
+                status[i] = 1;
+                if (boxFound[i]) queue.add(i);
+            }
+            for (int i : containedBoxes[cur]) {
+                boxFound[i] = true;
+                queue.add(i);
+            }
+        }
+        return ans;
+    }
+}
