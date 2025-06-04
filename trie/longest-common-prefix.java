5518920241cc07ebe606af9bc6253491bdb8aaf7
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-06-04 21:30:42 +0800

    LC-longest-common-prefix [Runtime: 1 ms; Memory: 41.4 MB]

--- trie/longest-common-prefix.java
+++ trie/longest-common-prefix.java
@@ -1,18 +1,14 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
         StringBuilder sb = new StringBuilder();
-        char c = ' ';
         for (int i = 0; i < strs[0].length(); i++) {
-            for (int j = 0; j < strs.length; j++) {
-                if (i >= strs[j].length()) return sb.toString();
-                if (j == 0) {
-                    c = strs[j].charAt(i);
-                    continue;
-                } 
-                if (strs[j].charAt(i) != c) return sb.toString();
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (i >= strs[j].length() || strs[j].charAt(i) != c) {
+                    return sb.toString();
+                }
             }
             sb.append(c);
-            c = ' ';
         }
         return sb.toString();
     }
