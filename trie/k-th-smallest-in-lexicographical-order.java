b7b72f03a8a9f6fd3a9be5fff8ab9ca2a89d9c2c
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-06-09 15:34:29 +0800

    LC-k-th-smallest-in-lexicographical-order [Runtime: N/A; Memory: N/A]

--- trie/k-th-smallest-in-lexicographical-order.java
+++ trie/k-th-smallest-in-lexicographical-order.java
@@ -1,19 +1,24 @@
 class Solution {
+    int count = 0;
+    int result = 0;
+
     public int findKthNumber(int n, int k) {
-        List<Integer> ans = new ArrayList<>();
         for (int i = 1; i <= 9 && i <= n; i++) {
-            dfs(i, n, ans);
-            if (ans.size() >= k) break;
+            dfs(i, n, k);
+            if (result != 0) break;
         }
-        return ans.get(k-1);
+        return result;
     }
 
-    private void dfs(int cur, int n, List<Integer> ans) {
-        if (cur > n) return;
-        ans.add(cur);
+    private void dfs(int cur, int n, int k) {
+        if (cur > n || result != 0) return;
+        count++;
+        if (count == k) {
+            result = cur;
+            return;
+        }
         for (int i = 0; i <= 9; i++) {
-            int next = cur * 10 + i;
-            dfs(next, n, ans);
+            dfs(cur * 10 + i, n, k);
         }
     }
 }
