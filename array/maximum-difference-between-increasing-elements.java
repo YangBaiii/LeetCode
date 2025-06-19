6d6082e854856e21389c4b281409a26434c756cb
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-06-20 00:35:46 +0800

    LC-maximum-difference-between-increasing-elements [Runtime: 1 ms; Memory: 42.1 MB]

--- array/maximum-difference-between-increasing-elements.java
+++ array/maximum-difference-between-increasing-elements.java
@@ -2,8 +2,9 @@ class Solution {
     public int maximumDifference(int[] nums) {
         int ans = -1, min = 1_000_000_001;
         for (int i = 0; i < nums.length; i++) {
-            ans = Math.max(ans, nums[i] - min);
             min = Math.min(min, nums[i]);
+            if (nums[i] <= min) continue;
+            ans = Math.max(ans, nums[i] - min);
         }
         return ans;
     }
