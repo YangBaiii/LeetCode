e4c1090845b518f40de44dc9cc20d87df9a07856
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-06-30 06:01:26 +0800

    LC-closest-binary-search-tree-value [Runtime: 0 ms; Memory: 42.9 MB]

--- binary-search-tree/closest-binary-search-tree-value.java
+++ binary-search-tree/closest-binary-search-tree-value.java
@@ -17,9 +17,11 @@ class Solution {
     public int closestValue(TreeNode root, double target) {
         int res = root.val;   
         while(root != null){
-            if(Math.abs(target - root.val) <= Math.abs(target - res)){
+            if(Math.abs(target - root.val) == Math.abs(target - res)){
                 res = Math.min(root.val, res);
-            }      
+            } else if(Math.abs(target - root.val) < Math.abs(target - res)){
+                res = root.val;
+            }           
             root = root.val > target? root.left: root.right;
         }     
         return res;
