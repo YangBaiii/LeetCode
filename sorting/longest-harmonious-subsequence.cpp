1c2c050fb4395e472ddf9bf5766b627b39dbb7ce
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-07-02 05:19:47 +0800

    LC-longest-harmonious-subsequence [Runtime: 45 ms; Memory: 44 MB]

--- sorting/longest-harmonious-subsequence.cpp
+++ sorting/longest-harmonious-subsequence.cpp
@@ -5,11 +5,10 @@ public:
         int ans = 0;
         for (auto i : nums) {
             map[i]++;
-            if (map.count(i+1)) {
+            if (map.count(i+1)) 
                 ans = max(ans, map[i] + map[i+1]);
-            } else if (map.count(i-1)) {
+            if (map.count(i-1)) 
                 ans = max(ans, map[i] + map[i-1]);
-            }
         }
         return ans;
     }
