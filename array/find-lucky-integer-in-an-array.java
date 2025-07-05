e4883c8ac8d1e969e7372f6855dcc43bddfd345f
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-07-05 21:32:13 +0800

    LC-find-lucky-integer-in-an-array [Runtime: 2 ms; Memory: 43.3 MB]

--- array/find-lucky-integer-in-an-array.java
+++ array/find-lucky-integer-in-an-array.java
@@ -1,12 +1,12 @@
 class Solution {
     public int findLucky(int[] arr) {
         int ans = -1;
-        int[] freq = new int[500];
+        int[] freq = new int[501];
         for (int i : arr) {
             freq[i]++;
         }   
         for (int i : freq) {
-            if (freq[i] > 0 && i == freq[i]) ans = i;
+            if (freq[i] > 0 && i == freq[i]) ans = Math.max(ans, i);
         }
         return ans;
     }
