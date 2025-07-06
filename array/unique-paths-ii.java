099ed6b3110a9bc860687d19e576fed1a20cde40
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-07-06 22:54:21 +0800

    LC-unique-paths-ii [Runtime: N/A; Memory: N/A]

--- array/unique-paths-ii.java
+++ array/unique-paths-ii.java
@@ -1,9 +1,11 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] grid) {
         int row = grid.length, col = grid[0].length;
+        if (grid[row-1][col-1] == 1 || grid[0][0] == 1) return 0;
         int[][] dp = new int[row][col];
-        for (int i = 0; i < row; i++) dp[i][0] = grid[i][0] == 1 ? 0 : 1;
-        for (int i = 0; i < col; i++) dp[0][i] = grid[0][i] == 1 ? 0 : 1;
+        dp[0][0] = 1;
+        for (int i = 1; i < row; i++) dp[i][0] = grid[i][0] == 1 ? 0 : 1;
+        for (int i = 1; i < col; i++) dp[0][i] = grid[0][i] == 1 ? 0 : 1;
 
         for (int i = 1; i < col; i++) {
             for (int j = 1; j < row; j++) {
