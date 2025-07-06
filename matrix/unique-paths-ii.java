3269c13be7c0b1e2e93b835ea8375b427423ef1d
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-07-06 23:02:39 +0800

    LC-unique-paths-ii [Runtime: 0 ms; Memory: 42 MB]

--- matrix/unique-paths-ii.java
+++ matrix/unique-paths-ii.java
@@ -4,14 +4,21 @@ class Solution {
         if (grid[row-1][col-1] == 1 || grid[0][0] == 1) return 0;
         int[][] dp = new int[row][col];
         dp[0][0] = 1;
-        for (int i = 1; i < row; i++) dp[i][0] = grid[i][0] == 1 ? 0 : dp[i-1][0];
-        for (int i = 1; i < col; i++) dp[0][i] = grid[0][i] == 1 ? 0 : dp[0][i-1];
-
+        
+        for (int i = 1; i < row; i++) {
+            dp[i][0] = grid[i][0] == 1 ? 0 : dp[i-1][0];
+        }
+    
         for (int i = 1; i < col; i++) {
-            for (int j = 1; j < row; j++) {
-                dp[i][j] = grid[i][j] == 1 ? 0 : dp[i-1][j] + dp[i][j-1];
+            dp[0][i] = grid[0][i] == 1 ? 0 : dp[0][i-1];
+        }
+
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < col; j++) {
+                dp[i][j] = grid[i][j] == 0 ? dp[i-1][j] + dp[i][j-1] : 0;
             }
         }
+
         return dp[row-1][col-1];
     }
 }
