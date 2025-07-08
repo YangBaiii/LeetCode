43050c718932e5c17397c984e2b43301a2ac70b4
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-07-09 03:03:05 +0800

    LC-find-k-closest-elements [Runtime: N/A; Memory: N/A]

---
+++ sliding-window/find-k-closest-elements.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int left = 0, right = arr.length - k;
+        List<Integer> ans = new ArrayList<>();
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if ((arr[mid] + arr[mid+k]) / 2 < x) left = mid + 1; 
+            else right = mid; 
+        }
+        for (int i = left; i < left + k; i++) ans.add(arr[i]);
+        return ans;
+        // return Arrays.stream(ans, left, left + k).boxed().collect(Collectors.toList());
+    }
+}
