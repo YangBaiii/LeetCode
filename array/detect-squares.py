83fb7d8cfd4a6988f1da52738bc739e9b4be229e
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-07-15 21:49:48 +0800

    LC-detect-squares [Runtime: 96 ms; Memory: 19.2 MB]

--- array/detect-squares.py
+++ array/detect-squares.py
@@ -1,20 +1,15 @@
 class DetectSquares:
     def __init__(self):
-        self.cnt = defaultdict(int)
+        self.cntPoints = Counter()
 
-    def add(self, point: list[int]) -> None:
-        self.cnt[tuple(point)] += 1
+    def add(self, point: List[int]) -> None:
+        self.cntPoints[tuple(point)] += 1
 
-    def count(self, point: list[int]) -> int:
-        x, y = point
+    def count(self, point: List[int]) -> int:
         ans = 0
-        for (x2, y2), freq in self.cnt.items():
-            if abs(x - x2) != abs(y - y2) or x == x2 or y == y2:
-                continue
-            ans += freq * self.cnt[(x, y2)] * self.cnt[(x2, y)]
+        x1, y1 = point
+        for (x3, y3), cnt in self.cntPoints.items():
+            if abs(x1 - x3) == 0 or abs(x1 - x3) != abs(y1 - y3):
+                continue  
+            ans += cnt * self.cntPoints[(x1, y3)] * self.cntPoints[(x3, y1)]
         return ans
-
-# Your DetectSquares object will be instantiated and called as such:
-# obj = DetectSquares()
-# obj.add(point)
-# param_2 = obj.count(point)
