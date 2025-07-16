aaece61d768d8573c9fef790df9af6a6c50894e6
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-07-16 15:32:15 +0800

    LC-jump-game [Runtime: N/A; Memory: N/A]

---
+++ array/jump-game.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        dis = 0
+        
+        for i in range(len(nums)):
+            if i > dis:
+                return False
+            dis = max(dis, i + nums[i])
+            if dis >= len(nums):
+                return True
+        
+        return dis >= len(nums)
