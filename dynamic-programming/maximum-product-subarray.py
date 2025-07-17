a41fe53c977c8e8939572df0a5995e431d3b13fd
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-07-17 18:32:34 +0800

    LC-maximum-product-subarray [Runtime: 11 ms; Memory: 19.2 MB]

--- dynamic-programming/maximum-product-subarray.py
+++ dynamic-programming/maximum-product-subarray.py
@@ -1,8 +1,7 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         pos, neg = [1] * len(nums), [1] * len(nums)
-        pos[0] = neg[0] = nums[0]
-        ans = 0
+        ans = pos[0] = neg[0] = nums[0]
 
         for i in range(1, len(nums)):
             pos[i] = max(pos[i-1] * nums[i], nums[i], neg[i-1] * nums[i])
