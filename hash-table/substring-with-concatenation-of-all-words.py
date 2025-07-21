67d0c78c779baa72c89f72e5326bd70172b7b99c
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-07-21 09:42:17 +0800

    LC-substring-with-concatenation-of-all-words [Runtime: 7408 ms; Memory: 18.4 MB]

--- hash-table/substring-with-concatenation-of-all-words.py
+++ hash-table/substring-with-concatenation-of-all-words.py
@@ -1,10 +1,24 @@
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
-        ans = []
-        perm = permutations(words)
-        for p in perm:
-            c = ''.join(p[:])
-            if s.find(c) >= 0 and s.find(c) not in ans:
-                ans.append(s.find(c))
+        word_count = defaultdict(int)
+        for word in words:
+            word_count[word] += 1
         
+        substr_len = len(words) * len(words[0])
+        word_len = len(words[0])
+        ans = []
+
+        for i in range(len(s) - substr_len + 1):
+            seen = defaultdict(int)
+            for j in range(i, i + substr_len, word_len):
+                word = s[j:j + word_len]
+                if word in word_count:
+                    seen[word] += 1
+                    if seen[word] > word_count[word]:
+                        break
+                else: break
+            else:
+                ans.append(i)
+                # loop didn't break 
+
         return ans
