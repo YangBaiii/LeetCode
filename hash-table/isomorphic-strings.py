57ad844dbabb31251a5e6052da8167840c7637a7
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-07-26 14:57:03 +0800

    LC-isomorphic-strings [Runtime: 3 ms; Memory: 18.2 MB]

--- hash-table/isomorphic-strings.py
+++ hash-table/isomorphic-strings.py
@@ -1,17 +1,13 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        mapping = defaultdict(str)
-        reverse_mapping = defaultdict(str)
-        for i in range(len(s)):
-            if s[i] not in mapping:
-                mapping[s[i]] = t[i]
-            if mapping[s[i]] != t[i]:
+        mapping = {}
+        for ds, dt in zip(s, t):
+            if ds in mapping:
+                if mapping[ds] != dt:
+                    return False
+            elif dt in mapping.values():
                 return False
 
-        for i in range(len(t)):
-            if t[i] not in reverse_mapping:
-                reverse_mapping[t[i]] = s[i]
-            if reverse_mapping[t[i]] != s[i]:
-                return False
-                
+            mapping[ds] = dt
+
         return True
