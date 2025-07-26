2075e004734bc46d60f9e48bc4f558683237bae9
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-07-26 14:41:50 +0800

    LC-isomorphic-strings [Runtime: 9 ms; Memory: 17.9 MB]

--- string/isomorphic-strings.py
+++ string/isomorphic-strings.py
@@ -1,9 +1,17 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         mapping = defaultdict(str)
+        reverse_mapping = defaultdict(str)
         for i in range(len(s)):
             if s[i] not in mapping:
                 mapping[s[i]] = t[i]
             if mapping[s[i]] != t[i]:
                 return False
+
+        for i in range(len(t)):
+            if t[i] not in reverse_mapping:
+                reverse_mapping[t[i]] = s[i]
+            if reverse_mapping[t[i]] != s[i]:
+                return False
+                
         return True
