3de260effc8f952876cb4c857809d5612dabd957
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-07-27 14:17:05 +0800

    LC-count-hills-and-valleys-in-an-array [Runtime: 0 ms; Memory: 17.7 MB]

--- array/count-hills-and-valleys-in-an-array.py
+++ array/count-hills-and-valleys-in-an-array.py
@@ -1,13 +1,11 @@
 class Solution:
     def countHillValley(self, nums: List[int]) -> int:
-        ans = 0
-        for i in range(1, len(nums)-1):
-            l = i - 1
-            while nums[i] == nums[i+1]:
-                i += 1
-            r = i + 1
-            
-            if (nums[i] > nums[l] and nums[i] > nums[r]) or (nums[i] < nums[l] and nums[i] < nums[r]):
-                ans += 1
+        nums = [nums[0]] + [nums[i] for i in range(1, len(nums)) if nums[i] != nums[i-1]]
 
+        ans = 0
+        for i in range(1, len(nums) - 1):
+            if ((nums[i] > nums[i-1] and nums[i] > nums[i+1]) 
+            or (nums[i] < nums[i-1] and nums[i] < nums[i+1])):
+                ans += 1
+                
         return ans
