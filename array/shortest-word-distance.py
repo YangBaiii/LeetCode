7b6970a3046ce3ffa077de1a03c80cae0f52f5aa
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-08-02 22:32:31 +0800

    LC-shortest-word-distance [Runtime: 3 ms; Memory: 20.2 MB]

--- array/shortest-word-distance.py
+++ array/shortest-word-distance.py
@@ -1,16 +1,13 @@
 class Solution:
     def shortestDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
-        o1, o2, ans = 30001, 30001, 30001
+        o1, o2, ans = -1, -1, len(wordsDict)
 
-        for i in range(len(wordsDict)):
-            s = wordsDict[i]
-            if s != word1 and s != word2:
-                continue
-            elif s == word1:
-                o1 = i 
-                ans = min(ans, abs(o1-o2))
-            else:
+        for i, s in enumerate(wordsDict):
+            if s == word1:
+                o1 = i
+            elif s == word2:
                 o2 = i
-                ans = min(ans, abs(o1-o2))
-        
+            if o1 >= 0 and o2 >= 0:
+                ans = min(ans, abs(o1 - o2))
+
         return ans
