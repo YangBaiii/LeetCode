989c0137bcfdabcde920816f26be5f7f2d3fb824
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-08-02 18:40:14 +0800

    LC-subsets-ii [Runtime: 0 ms; Memory: 18 MB]

---
+++ array/subsets-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        ans, path = [], []
+        n = len(nums)
+
+        def backtrack(start):
+            ans.append(path[:])
+
+            for i in range(start, n):
+                if i > start and nums[i] == nums[i-1]:
+                    continue
+                path.append(nums[i])
+                backtrack(i+1)
+                path.pop()
+        
+        backtrack(0)
+        return ans
