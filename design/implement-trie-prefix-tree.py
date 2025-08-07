5f162cb12ea65c7bf6151c8fda431bdfb2e35fd4
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-08-07 17:21:15 +0800

    LC-implement-trie-prefix-tree [Runtime: 44 ms; Memory: 32.2 MB]

--- design/implement-trie-prefix-tree.py
+++ design/implement-trie-prefix-tree.py
@@ -1,24 +1,39 @@
-class Trie:
-
+class TrieNode:
     def __init__(self):
-        self.words = []
+        self.children = {}
+        self.isEnd = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
 
     def insert(self, word: str) -> None:
-        self.words.append(word)
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                node.children[c] = TrieNode()
+            node = node.children[c]
+        node.isEnd = True
 
     def search(self, word: str) -> bool:
-        for s in self.words:
-            if s == word:
-                return True
-        
-        return False
+        return self.dfs(word, 0, self.root, requireEnd=True)
 
     def startsWith(self, prefix: str) -> bool:
-        for s in self.words:
-            if s[:len(prefix)] == prefix:
-                return True
-        
-        return False
+        return self.dfs(prefix, 0, self.root, requireEnd=False)
+
+    def dfs(self, s, i, node, requireEnd):
+        if i == len(s):
+            return node.isEnd if requireEnd else True
+
+        c = s[i]
+        if c in node.children:
+            return self.dfs(s, i + 1, node.children[c], requireEnd)
+        else:
+            return False
+
+            
+
+
 
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()
