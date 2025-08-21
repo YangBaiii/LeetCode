71148478a039cf4f677cf03e4091f6c11ae96eca
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-08-22 07:46:51 +0800

    LC-insert-interval [Runtime: 0 ms; Memory: 19.6 MB]

--- array/insert-interval.py
+++ array/insert-interval.py
@@ -1,19 +1,14 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        n = len(intervals)
-        res = []
+        intervals.append(newInterval)
+        intervals.sort()
 
-        i = 0
-        while i < n and intervals[i][1] < newInterval[0]:
-            res.append(intervals[i])
-            i += 1
+        res = [intervals[0]]
 
-        while i < n and intervals[i][0] <= newInterval[1]:
-            newInterval[0] = min(newInterval[0], intervals[i][0])
-            newInterval[1] = max(newInterval[1], intervals[i][1])
-            i += 1
-
-        res.append(newInterval)
-        res.extend(intervals[i:])
-
+        for i in range(1, len(intervals)):
+            if res[-1][1] < intervals[i][0]:
+                res.append(intervals[i])
+            else:
+                res[-1][1] = max(res[-1][1], intervals[i][1])
+                
         return res
