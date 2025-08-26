bbbfd069b6373a670020a73fbb88ceb8c3fa11ac
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-08-27 03:09:53 +0800

    LC-basic-calculator-ii [Runtime: 58 ms; Memory: 21.4 MB]

---
+++ string/basic-calculator-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        s = s.replace(' ', '')
+        stack = []
+        num, sign = 0, '+'
+
+        for i, ch in enumerate(s):
+            if ch.isdigit():
+                num = num * 10 + int(ch)
+            if ch in '+-*/' or i == len(s) - 1:
+                if sign == '+':
+                    stack.append(num)
+                elif sign == '-':
+                    stack.append(-num)
+                elif sign == '*':
+                    stack.append(stack.pop() * num)
+                else:
+                    stack.append(int(stack.pop() / num))
+                num = 0
+                sign = ch
+                
+        return sum(stack)
