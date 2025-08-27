adb5e7364f495e34d551e54f2f2eccd1b239ca2e
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-08-27 13:50:39 +0800

    LC-course-schedule-ii [Runtime: N/A; Memory: N/A]

---
+++ breadth-first-search/course-schedule-ii.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findOrder(self, n: int, prerequisites: List[List[int]]) -> List[int]:
+        adj = [[] for _ in range(n)]
+        indegree = [0] * n
+        queue = deque()
+        ans = []
+
+        for l in prerequisites:
+            adj[l[1]].append(l[0])
+            indegree[l[0]] += 1
+        
+        for i in range(n):
+            if indegree[i] == 0:
+                queue.append(i)
+        
+        while queue:
+            cur = queue.popleft()
+            ans.append(cur)
+            for nxt in adj[cur]:
+                indegree[nxt] -= 1
+                if indegree[nxt] == 0:
+                    queue.append(nxt)
+        
+        return ans
+            
