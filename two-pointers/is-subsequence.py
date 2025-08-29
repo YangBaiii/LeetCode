5771d374b96cecdeb7bf598ef34928f61f00ee6f
Baiiiiii <114151438+YangBaiii@users.noreply.github.com>
2025-08-30 04:01:47 +0800

    LC-is-subsequence [Runtime: N/A; Memory: N/A]

---
+++ two-pointers/is-subsequence.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        l, r = 0, 0
+        if len(s) > len(t) and len(t) == 0:
+            return False
+        if len(s) == 0:
+            return True
+            
+        while r < len(t):
+            if t[r] == s[l]:
+                l += 1
+                if l == len(s) - 1:
+                    return True
+            r += 1
+        
+        return False
